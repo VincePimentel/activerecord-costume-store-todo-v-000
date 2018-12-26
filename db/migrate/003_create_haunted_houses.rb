2cfab0949a6d3e35fbfc005529c3fc0179c54533
Vince Andrew Pimentel <contact@vincep.me>
2018-12-26 03:02:39 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,3 +1,10 @@
 class HauntedHouses < ActiveRecord::Migration
-
+  def change
+    create_table :haunted_houses do |c|
+      c.string :name
+      c.string :location
+      c.string :theme
+      c.integer :price
+    end
+  end
 end
